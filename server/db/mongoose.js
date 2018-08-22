d2e4728f4023387d15a7382ed6c63373b3c37664
dbEsq <dougmberman@gmail.com>
2018-08-22 00:14:06 -0500

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise - global.Promise;
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://dbEsq:pswd136@ds227322.mlab.com:27322/nodejs');
 
 module.exports  = {
