2bf1a749ee03cd1c166cd22bc149cc1c33d27a87
dbEsq <dougmberman@gmail.com>
2018-08-23 01:24:09 -0500

    Mongoose Connect edit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://dbEsq:pswd136@ds227322.mlab.com:27322/nodejs');
 
 module.exports  = {
   mongoose
