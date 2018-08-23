44384101470201d7dce994b605f950a5fbaed128
dbEsq <dougmberman@gmail.com>
2018-08-23 00:59:00 -0500

    Add tests for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todos});
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
