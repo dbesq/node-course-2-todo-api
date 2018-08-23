6b806efe3b437e136b2edee1777931849afa853a
dbEsq <dougmberman@gmail.com>
2018-08-23 01:19:03 -0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://dbEsq:pswd136@ds227322.mlab.com:27322/nodejs');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports  = {
   mongoose
