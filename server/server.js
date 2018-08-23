d72caf78a774f427ac39d0c8104ace65600c151a
dbEsq <dougmberman@gmail.com>
2018-08-22 23:57:24 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -65,6 +66,33 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);  //What users can update
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  //Update completedAt property
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();  //returns JS timestamp
+  } else{
+    body.completed = false;
+    body.completedAt = null; // Set property to null to remove from database
+  }
+
+  //call findByIdAndUpdate
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+});
 
 
 app.listen(port, () => {
