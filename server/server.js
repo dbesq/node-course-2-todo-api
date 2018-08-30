45f90c4c18dae114616896ff7e3684713ef16a0d
dbEsq <dougmberman@gmail.com>
2018-08-30 01:13:02 -0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -96,6 +96,25 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+// POST /users
+ //use pick liek with patch to get body variable to pass into contructor function
+ // made changes to user model so need to:
+    // 1. wipe database
+    // 2.  restart database.
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
