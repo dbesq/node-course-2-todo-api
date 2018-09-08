d2b5ab994a51d63ce249a27539aa7471e097c169
dbEsq <dougmberman@gmail.com>
2018-09-07 19:21:34 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -97,6 +97,7 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -108,6 +109,19 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+// POST /users/login sending (email, password)
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 
